3fc02324c7a2860ddef746d03117fdcb796b9ead
Chienhung Lin <ljredracess@gmail.com>
2024-07-21 00:06:56 +0800

    Init project by flutter cmd

---
+++ android/app/src/main/kotlin/app/dledger/dledger_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.dledger.dledger_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ dledger/example/dledger_example.dart
@@ -0,0 +1,6 @@
+import 'package:dledger/dledger.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
