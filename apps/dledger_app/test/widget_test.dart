5026152abe791362284070f6cb38a00a847d078d
Chienhung Lin <ljredracess@gmail.com>
2024-08-01 09:54:32 +0800

    Add a smoke test for DLedgerApp

--- apps/dledger_app/test/widget_test.dart
+++ apps/dledger_app/test/widget_test.dart
@@ -8,21 +8,12 @@
 import 'package:dledger_app/src/dledger_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const DLedgerApp());
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.byKey(const Key('dledger-app')), findsOneWidget);
-
-    //// Tap the '+' icon and trigger a frame.
-    //await tester.tap(find.byIcon(Icons.add));
-    //await tester.pump();
-
-    //// Verify that our counter has incremented.
-    //expect(find.text('0'), findsNothing);
-    //expect(find.text('1'), findsOneWidget);
+  testWidgets('smoke test, verify app can run', (WidgetTester tester) async {
+    runApp(const DLedgerApp());
   });
 }
