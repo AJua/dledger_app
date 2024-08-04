60a5e0a419eccbbbde5ba7cc67811a7fd47422de
Chienhung Lin <ljredracess@gmail.com>
2024-08-04 20:02:47 +0800

    Impletement statements.add logic

--- packages/dledger_lib/lib/src/aggregates/statements.dart
+++ packages/dledger_lib/lib/src/aggregates/statements.dart
@@ -4,7 +4,7 @@ import 'package:equatable/equatable.dart';
 
 class Statements extends Equatable {
   final Map<Account, Map<StatementPeriod, Commodities>> _all;
-  final PeriodType type;
+  final PeriodType _periodType;
 
   final Set<StatementPeriod> _periods;
 
@@ -12,7 +12,7 @@ class Statements extends Equatable {
 
   Map<Account, Map<StatementPeriod, Commodities>> get all => _all;
 
-  Statements._(this.type)
+  Statements._(this._periodType)
       : _all = {},
         _periods = {};
 
@@ -24,13 +24,26 @@ class Statements extends Equatable {
   List<Object?> get props => [all];
 
   void add(Posting posting) {
+    var period = StatementPeriod(posting.date, _periodType);
+    _periods.add(period);
     if (_all.containsKey(posting.account)) {
-      //var period = posting.date.
+      if (_all[posting.account]!.containsKey(period)) {
+        var commodities = _all[posting.account]![period];
+        commodities!.add(posting.commodity);
+      } else {
+        var commodities = Commodities.empty()..add(posting.commodity);
+        _all[posting.account]![period] = commodities;
+      }
     } else {
       var commodities = Commodities.empty()..add(posting.commodity);
-      _all[posting.account] = {
-        StatementPeriod(posting.date, type): commodities,
-      };
+      _all[posting.account] = {period: commodities};
+    }
+    for (var p in _periods) {
+      for (var account in _all.keys) {
+        if (!_all[account]!.containsKey(p)) {
+          _all[account]![p] = Commodities.empty();
+        }
+      }
     }
   }
 }
--- packages/dledger_lib/test/aggregates/statements_test.dart
+++ packages/dledger_lib/test/aggregates/statements_test.dart
@@ -1,18 +1,114 @@
 import 'package:dledger_lib/dledger_lib.dart';
+import 'package:dledger_lib/src/aggregates/commodities.dart';
 import 'package:dledger_lib/src/aggregates/statements.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
-  test('commodity with same unit', () {
+  test('add a single posting', () {
     var statements = Statements.empty(PeriodType.daily);
-    var c = const Commodity(100, '\$', UnitPosition.left);
-    var p = Posting(DateTime(2024, 1, 24), const Account(['expense', 'foods']), c);
+    const account = Account(['expense', 'foods']);
+    const commodity = Commodity(100, '\$', UnitPosition.left);
+    var p = Posting(
+      DateTime(2024, 1, 24),
+      account,
+      commodity,
+    );
 
     statements.add(p);
 
-    expect({
-      StatementPeriod(DateTime(2024, 8, 1), PeriodType.monthly),
-      StatementPeriod(DateTime(2024, 8, 3), PeriodType.monthly)
-    }, equals({StatementPeriod(DateTime(2024, 8, 2), PeriodType.monthly)}));
+    expect(
+      statements.all,
+      equals({
+        account: {StatementPeriod(p.date, PeriodType.daily): Commodities.empty()..add(commodity)}
+      }),
+    );
+  });
+  test('add 2 posting with different accounts', () {
+    var statements = Statements.empty(PeriodType.daily);
+    const account1 = Account(['expense', 'foods']);
+    const account2 = Account(['expense', 'clothes']);
+    const commodity = Commodity(100, '\$', UnitPosition.left);
+    var p1 = Posting(DateTime(2024, 1, 24), account1, commodity);
+    var p2 = Posting(DateTime(2024, 1, 24), account2, commodity);
+
+    statements.add(p1);
+    statements.add(p2);
+
+    expect(
+      statements.all,
+      equals({
+        account1: {StatementPeriod(p1.date, PeriodType.daily): Commodities.empty()..add(commodity)},
+        account2: {StatementPeriod(p2.date, PeriodType.daily): Commodities.empty()..add(commodity)}
+      }),
+    );
+  });
+  test('add 2 posting with same account and same date', () {
+    var statements = Statements.empty(PeriodType.daily);
+    const account = Account(['expense', 'foods']);
+    const commodity1 = Commodity(100, '\$', UnitPosition.left);
+    const commodity2 = Commodity(200, '\$', UnitPosition.left);
+    var p1 = Posting(DateTime(2024, 1, 24), account, commodity1);
+    var p2 = Posting(DateTime(2024, 1, 24), account, commodity2);
+
+    statements.add(p1);
+    statements.add(p2);
+
+    expect(
+      statements.all,
+      equals({
+        account: {
+          StatementPeriod(p1.date, PeriodType.daily): Commodities.empty()
+            ..add(commodity1)
+            ..add(commodity2),
+        },
+      }),
+    );
+  });
+  test('add 2 posting with same account and different date', () {
+    var statements = Statements.empty(PeriodType.daily);
+    const account = Account(['expense', 'foods']);
+    const commodity1 = Commodity(100, '\$', UnitPosition.left);
+    const commodity2 = Commodity(200, '\$', UnitPosition.left);
+    var p1 = Posting(DateTime(2024, 1, 24), account, commodity1);
+    var p2 = Posting(DateTime(2024, 1, 25), account, commodity2);
+
+    statements.add(p1);
+    statements.add(p2);
+
+    expect(
+      statements.all,
+      equals({
+        account: {
+          StatementPeriod(p1.date, PeriodType.daily): Commodities.empty()..add(commodity1),
+          StatementPeriod(p2.date, PeriodType.daily): Commodities.empty()..add(commodity2),
+        },
+      }),
+    );
+  });
+  test('add 2 posting with different account and different date', () {
+    var statements = Statements.empty(PeriodType.daily);
+    const account1 = Account(['expense', 'foods']);
+    const account2 = Account(['expense', 'clothes']);
+    const commodity1 = Commodity(100, '\$', UnitPosition.left);
+    const commodity2 = Commodity(200, '\$', UnitPosition.left);
+    var p1 = Posting(DateTime(2024, 1, 24), account1, commodity1);
+    var p2 = Posting(DateTime(2024, 1, 25), account2, commodity2);
+
+    statements.add(p1);
+    statements.add(p2);
+
+    expect(
+      statements.all,
+      equals({
+        account1: {
+          StatementPeriod(p1.date, PeriodType.daily): Commodities.empty()..add(commodity1),
+          StatementPeriod(p2.date, PeriodType.daily): Commodities.empty(),
+        },
+        account2: {
+          StatementPeriod(p1.date, PeriodType.daily): Commodities.empty(),
+          StatementPeriod(p2.date, PeriodType.daily): Commodities.empty()..add(commodity2),
+        },
+      }),
+    );
   });
 }
